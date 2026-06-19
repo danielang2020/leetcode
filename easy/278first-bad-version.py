80e9d7aca270b0a63261e59d63e139c93243bb12
Daniel.Lang <daniel006@uuubee.com>
2026-06-19 17:29:58 +0800

    20260619

---
+++ easy/278first-bad-version.py
@@ -0,0 +1,31 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+import unittest
+
+class Solution:
+    def isBadVersion(self,version: int) -> bool:
+        return version >= 3
+        
+    def firstBadVersion(self, n: int) -> int:
+        low = 1
+        high = n
+
+        while low < high:
+            mid = (high + low) // 2
+            bad = self.isBadVersion(mid)
+            if bad:
+                high = mid
+            else:
+                low = mid + 1
+        return low
+                
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.instance = Solution()
+
+    def test_search(self):
+        self.assertEqual(self.instance.firstBadVersion(5),3)
+
+
+if __name__ == "__main__":
+    unittest.main()
